58019ad120aad23b4cda24ee24fc2d7ad9ae9ce4
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-30 23:50:52 +0600

    added styled-components

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+	background-color: #d74234;
+	min-height: 550px;
+	padding: 100px 0;
+	color: #fff;
+`
+
+const Header = styled.h1`
+	color: #fff;
+	font-weight: 700;
+	font-size: 40px;
+	line-height: 52px;
+`
+
+const SubHead = styled.p`
+	font-size: 18px;
+	font-weight: 500;
+`
+
 
 const Jumbotron = () => {
 	return (
-		<section className="home-section--1">
+		<Section className="home-section--1">
 			<div className="container">
 				<div className="row">
 					<div className="col col-sm-12 col-md-5">
 						<div className="pt-4 mt-4">
-							<h1>Digital Painting and Drawing For Developers</h1>
-							<p>Supercharge your drawing skills</p>
+							<Header>Digital Painting and Drawing For Developers</Header>
+							<SubHead>Supercharge your drawing skills</SubHead>
 						</div>
 					</div>
 					<div className="col col-sm-12 col-md-7">
@@ -18,7 +39,7 @@ const Jumbotron = () => {
 					</div>
 				</div>
 			</div>
-		</section>
+		</Section>
 	)
 }
 
