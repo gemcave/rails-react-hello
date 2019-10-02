24979dc1cc05a97b57e60927d4c736f7e63aff75
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-03 00:09:22 +0600

    small api fix and axios fetch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
 	def episodes
 		@episodes = Course.first.episodes
 
-		render json: { data: @pisodes }
+		render json: { data: @episodes }
   end
 end
--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import Jumbotron from './Jumbotron'
 import Table from './Table/Table'
+import axios from 'axios'
 
 class Home extends Component {
 	constructor() {
 		super()
 		
 		this.state = {
-			course_modules: [
-				{ id: 1, title: '1. Paint Structure', description: 'lorem ipsum', active: false },
-				{ id: 2, title: '2. Paint Anatomy', description: 'lorem ipsum', active: false },
-				{ id: 3, title: '3. Paint Head', description: 'lorem ipsum', active: false },
-				{ id: 4, title: '4. Paint Hands', description: 'lorem ipsum', active: false }, 
-			] 
+			course_modules: [] 
 		}
 	} 
+
+	componentDidMount() {
+		axios.get('/episodes.json')
+		.then(data => {
+			let res = []
+			data.data.data.map((data) => {
+				res.push({id: data.id, title: data.title, description: data.description, active: false })
+				this.setState({course_modules: res})
+			})
+		})
+		.catch(data => {
+
+		})
+	}
 
 	handleVideoChange(item, e) {
 		e.preventDefault()
