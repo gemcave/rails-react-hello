549f25fba054a4b79cda05fb99c7dae509b891d1
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-02 23:05:54 +0600

    video item component

---
+++ app/javascript/components/Home/Table/Item.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+const Item =  (props) => {
+	return (
+		<div className="row">
+			<div className="col-md-10 offset-md-1">
+				<div className="text-center">
+					<div className="card px-5">
+						<div className="row">
+							<div className="col-md-4">
+							</div>
+							<div className="col-md-8">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
